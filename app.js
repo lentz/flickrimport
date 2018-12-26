fd02396c327daf6f78cea9373fe1f368bd7ed477
Jesse Lentz <jesselentz@gmail.com>
2018-12-25 22:56:11 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const querystring = require('querystring');
+const url = require('url');
+const { OAuth2Client } = require('google-auth-library');
+require('dotenv').config();
+
+const { importPhotos } = require('./index');
+
+async function authMiddleware(req, res) {
+  const qs = querystring.parse(url.parse(req.url).query);
+  const oAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    [process.env.REDIRECT_URI],
+  );
+
+  if (req.headers.cookie) {
+    const tokens = JSON.parse(req.headers.cookie.split('=')[1]);
+    oAuth2Client.setCredentials(tokens);
+  } else if (qs.code) {
+    const tokensResp = await oAuth2Client.getToken(qs.code)
+    oAuth2Client.setCredentials(tokensResp.tokens);
+    res.setHeader(
+      'Set-Cookie',
+      `token=${JSON.stringify(tokensResp.tokens)}; Max-Age=2147483647;  HttpOnly`,
+    );
+  } else {
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/photoslibrary',
+      prompt: 'consent',
+    });
+    res.setHeader('Location', authorizeUrl);
+    res.statusCode = 302;
+    return res.end();
+  }
+
+  importPhotos(oAuth2Client);
+}
+
+const server = http.createServer(async (req, res) => {
+  try {
+    const parsedUrl = url.parse(req.url);
+    await authMiddleware(req, res);
+    if (parsedUrl.pathname === '/' && !res.finished) {
+      res.end('Beginning import');
+    }
+    else {
+      res.statusCode = 404;
+      res.end();
+    }
+  } catch (err) {
+    res.statusCode = 500;
+    res.end(err.toString());
+  }
+}).listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT);
+});
---
+++ index.js
@@ -0,0 +1,113 @@
+const { readFileSync } = require('fs');
+const glob = require('glob');
+const debug = require('debug')('photosupload');
+const path = require('path');
+const _ = require('lodash');
+require('dotenv').config();
+
+const albumItemCache = {};
+
+async function getItemsForAlbum(oAuth2Client, albumId) {
+  if (albumItemCache[albumId]) { return albumItemCache[albumId]; }
+
+  let pageToken;
+  do {
+    const albumContentsRes = await oAuth2Client.request({
+      method: 'post',
+      data: { pageSize: 100, pageToken },
+      url: 'https://photoslibrary.googleapis.com/v1/mediaItems:search',
+    });
+    pageToken = albumContentsRes.data.nextPageToken;
+    albumItemCache[albumId] = (albumItemCache[albumId] || []).concat(albumContentsRes.data.mediaItems);
+  } while(pageToken);
+
+  return albumItemCache[albumId];
+}
+
+async function uploadPhoto(oAuth2Client, flickrPhotoId, albumId) {
+  try {
+    const files = glob.sync(path.join(process.env.PHOTOS_DIR, `*${flickrPhotoId}*.jpg`));
+    if (!files.length) { 
+      console.error(`Photo not found with ID ${flickrPhotoId} for album ${albumId}!`);
+      return;
+    }
+    const photoPath = files[0];
+
+    debug(`Uploading ${path.basename(photoPath)}`);
+
+    const uploadRes = await oAuth2Client.request({
+      url: 'https://photoslibrary.googleapis.com/v1/uploads',
+      method: 'post',
+      data: readFileSync(photoPath),
+      headers: {
+        'Content-type': 'application/octet-stream',
+        'X-Goog-Upload-File-Name': path.basename(photoPath),
+        'X-Goog-Upload-Protocol': 'raw',
+      },
+    });
+
+    const addToAlbumRes = await oAuth2Client.request({
+      data: {
+        albumId,
+        newMediaItems: [{ simpleMediaItem: { uploadToken: uploadRes.data } }],
+      },
+      method: 'post',
+      url: 'https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate',
+    });
+
+    const result = addToAlbumRes.data.newMediaItemResults[0].status;
+    if (result.message !== 'OK') { throw new Error(`Upload failed for ${flickrPhotoId}: ${result}`); }
+  } catch(err) {
+    console.error(`Error uploading photo ${path.basename(photoPath)}: ${err.stack}`);
+  }
+}
+
+async function getAlbums(oAuth2Client) {
+  let albums = [];
+  let pageToken;
+  do {
+    const albumsRes = await oAuth2Client.request({
+      params: { pageSize: 50, pageToken },
+      url: 'https://photoslibrary.googleapis.com/v1/albums',
+    });
+    albums = albums.concat(albumsRes.data.albums);
+    pageToken = albumsRes.data.nextPageToken;
+  } while (pageToken);
+  
+  debug(`Found ${albums.length} Google albums`);
+  return albums;
+}
+
+async function importPhotos(oAuth2Client) {
+  try {
+    const flickrAlbums = JSON.parse(readFileSync(path.join(process.env.METADATA_DIR, 'albums.json')));
+    debug(`Importing ${flickrAlbums.albums.length} Flickr albums`);
+    const googleAlbums = await getAlbums(oAuth2Client);
+
+    for (const flickrAlbum of flickrAlbums.albums) {
+      console.log(`Uploading ${flickrAlbum.photos.length} photos to ${flickrAlbum.title}`);
+      let googleAlbum = googleAlbums.find(album => flickrAlbum.title === album.title);
+      if (!googleAlbum) {
+        debug(`Creating album for ${flickrAlbum.title}`)
+        const createAlbumRes = await oAuth2Client.request({
+          data: { album: { title: flickrAlbum.title } },
+          method: 'post',
+          url: 'https://photoslibrary.googleapis.com/v1/albums',
+        });
+        googleAlbum = createAlbumRes.data;
+      }
+
+      const photoChunks = _.chunk(flickrAlbum.photos, 5);
+      for (const photoChunk of photoChunks) {
+        await Promise.all(photoChunk.map(photoId => uploadPhoto(oAuth2Client, photoId, googleAlbum.id)));
+      }
+      console.log(`Completed import for ${flickrAlbum.title}`);
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+module.exports = {
+  importPhotos,
+};
